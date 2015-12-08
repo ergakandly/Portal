1923342caf23041d4e9f51f203444a77af88961f
Virgiawan <virgiawanap@gmail.com>
2015-12-08 14:14:18 +0700

    chart

--- src/com/hris/portal/manager/ChartManager.java
+++ src/com/hris/portal/manager/ChartManager.java
@@ -28,4 +28,46 @@ public class ChartManager {
 		
 		return result;
 	}
+	
+	public List<ChartBean> getPastLate()
+	{
+		List<ChartBean> result = null;
+		
+		try {
+			result = ibatis.queryForList("charts.getPastLate", "");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	public List<ChartBean> getPastWorkingHours()
+	{
+		List<ChartBean> result = null;
+		
+		try {
+			result = ibatis.queryForList("charts.getPastWorkingHours", "");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	public List<ChartBean> getPastLeave()
+	{
+		List<ChartBean> result = null;
+		
+		try {
+			result = ibatis.queryForList("charts.getPastLeave", "");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
 }
